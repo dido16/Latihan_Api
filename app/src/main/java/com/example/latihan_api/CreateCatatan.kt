68f50123e37d21172f9dcd918b11be7b817a0c62
dido16 <didofajar16@gmail.com>
2025-12-01 16:22:23 +0700

    Membuat Create Activity

---
+++ app/src/main/java/com/example/latihan_api/CreateCatatan.kt
@@ -0,0 +1,74 @@
+package com.example.latihan_api
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Message
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import com.example.latihan_api.databinding.ActivityCreateCatatanBinding
+import com.example.latihan_api.entities.Catatan
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.plus
+
+class CreateCatatan : AppCompatActivity() {
+    private lateinit var binding: ActivityCreateCatatanBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityCreateCatatanBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        setupEvent()
+
+    }
+
+    fun setupEvent() {
+        binding.TombolSimpan.setOnClickListener{
+            val judul = binding.InputJudul.text.toString()
+            val isi = binding.InputIsi.text.toString()
+
+            if (judul.isEmpty() || isi.isEmpty())  {
+                displayMessage("Judul dan Isi Catatan Harus diisi")
+                return@setOnClickListener
+            }
+
+            val payload = Catatan(
+                id = null,
+                judul = judul,
+                isi = isi,
+                user_id = 1
+            )
+
+            lifecycleScope.launch{
+                val response = RetrofitClient.catatanRepository.createCatatan(payload)
+                if (response.isSuccessful) {
+                    displayMessage("Catatan Berhasil Dibuat")
+
+                    val intent = Intent(this@CreateCatatan, MainActivity::class.java)
+                    startActivity(intent)
+
+                    finish()
+                } else {
+                    displayMessage("Gagal : ${response.message()}")
+                }
+            }
+        }
+    }
+
+    fun displayMessage(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+}
---
+++ app/src/main/java/com/example/latihan_api/entities/Catatan.kt
@@ -0,0 +1,8 @@
+package com.example.latihan_api.entities
+
+data class Catatan(
+    val id: Int? = null,
+    val judul: String,
+    val isi: String,
+    val user_id: Int
+)
