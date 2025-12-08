524af2ee9bb77d193142320530530887b3f7d39d
dido16 <didofajar16@gmail.com>
2025-12-08 10:16:44 +0700

    Menambahkan GET

--- app/src/main/java/com/example/latihan_api/RetrofitClient.kt
+++ app/src/main/java/com/example/latihan_api/RetrofitClient.kt
@@ -5,7 +5,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://192.168.0.102:8000/api/"
+    private const val BASE_URL = "http://192.168.18.156:8000/api/"
 
     val catatanRepository: CatatanRepository by lazy {
         Retrofit.Builder()
